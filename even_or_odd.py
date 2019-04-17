47132985ef537bfe8baa7a14414ba1921af44008
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-16 22:55:09 -0500

    Add files via upload

---
+++ even_or_odd.py
@@ -0,0 +1,11 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 ==0:
+	print("\nThe number " + str(number) + " is even.")
+else:
+	print("\nThe number " + str(number) + " is odd.")
